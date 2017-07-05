ba8e9bc74377871372563129ae241425816fa263
maximest-pierre <me@maximest-pierre.me>
2017-07-05 15:06:57 -0400

    Fix serializers model problem

--- api/serializers.py
+++ api/serializers.py
@@ -6,20 +6,20 @@ from .models import Entity, Human, Object
 class EntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entity
-        fields = ('name', 'time')
+        fields = ('name', 'time', 'x_position', 'y_position', 'z_position')
 
 
 class HumanSerializer(serializers.ModelSerializer):
     entity = EntitySerializer(read_only=True)
 
     class Meta:
-        model = Entity
-        fields = ('entity')
+        model = Human
+        fields = ('entity', 'gender', 'person_name')
 
 
 class ObjectSerializer(serializers.ModelSerializer):
     entity = EntitySerializer(read_only=True)
 
     class Meta:
-        model = Entity
-        fields = ('entity')
+        model = Object
+        fields = ('entity', 'type', 'color')
