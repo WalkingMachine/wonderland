cc85c9aad408aaa3ef4a77053ba02ddc31ae306f
maximest-pierre <me@maximest-pierre.me>
2017-07-05 00:12:41 -0400

    Add more fields to the model

--- api/models.py
+++ api/models.py
@@ -6,13 +6,20 @@ class Entity(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=60, default='')
     time = models.DateTimeField(null=True)
+    x_position = models.FloatField(null=True)
+    y_position = models.FloatField(null=True)
+    z_position = models.FloatField(null=True)
 
 
 class Human(models.Model):
     id = models.AutoField(primary_key=True)
     entity = models.ForeignKey(Entity, null=True)
+    gender = models.CharField(max_length=1, null=True)
+    person_name = models.CharField(max_length=60, null=True)
 
 
 class Object(models.Model):
     id = models.AutoField(primary_key=True)
     entity = models.ForeignKey(Entity, null=True)
+    type = models.CharField(max_length=20, null=True)
+    color = models.CharField(max_length=20, null=True)
