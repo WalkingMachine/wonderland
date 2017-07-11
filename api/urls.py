c7f8d52a5eee9597cacb1877480d980ff2b0bcf3
maximest-pierre <me@maximest-pierre.me>
2017-07-11 06:41:48 -0400

    Add urls for room and waypoint

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from api import views
 urlpatterns = [
     url(r'^api/entity/$', views.EntityList.as_view()),
     url(r'^api/human/$', views.HumanList.as_view()),
-    url(r'^api/object/$', views.ObjectList.as_view())
+    url(r'^api/object/$', views.ObjectList.as_view()),
+    url(r'^api/room/$', views.RoomList.as_view()),
+    url(r'^api/waypoint/$', views.WaypointList.as_view()),
 ]
