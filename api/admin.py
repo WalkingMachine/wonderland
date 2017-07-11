1d48db0a4c7216e95d110f7587131901982755b7
maximest-pierre <me@maximest-pierre.me>
2017-07-10 20:18:54 -0400

    Add admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.models import Entity, Object, Human
 
 # Register your models here.
+admin.site.register(Entity)
+admin.site.register(Object)
+admin.site.register(Human)
