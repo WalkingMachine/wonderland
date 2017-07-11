27736feb7fa260642a74041301663866f947e646
maximest-pierre <me@maximest-pierre.me>
2017-07-10 20:18:54 -0400

    Add admin panel

--- api/models.py
+++ api/models.py
@@ -10,6 +10,9 @@ class Entity(models.Model):
     y_position = models.FloatField(null=True)
     z_position = models.FloatField(null=True)
 
+    def __str__(self):
+        return "{}".format(self.name)
+
 
 class Human(models.Model):
     id = models.AutoField(primary_key=True)
@@ -17,9 +20,14 @@ class Human(models.Model):
     gender = models.CharField(max_length=1, null=True)
     person_name = models.CharField(max_length=60, null=True)
 
+    def __str__(self):
+        return "{}".format(self.person_name)
 
 class Object(models.Model):
     id = models.AutoField(primary_key=True)
     entity = models.ForeignKey(Entity, null=True)
     type = models.CharField(max_length=20, null=True)
     color = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return "{}".format(self.type)
--- wonderland/urls.py
+++ wonderland/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.conf.urls import url, include
 from rest_framework import routers
 from api import views
@@ -25,4 +25,5 @@ urlpatterns = [
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('api.urls')),
+    url(r'^admin/', admin.site.urls)
 ]
