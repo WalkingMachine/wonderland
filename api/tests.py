2d5e75bcd2927413f09f161dac08f1ea60694636
Lucas Maurice <lucas.maurice@outlook.com>
2018-03-25 16:08:46 -0400

    Clear unit tests.

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from api.models import Entity, Human, Object, ArTag
+from api.models import Entity
 # Create your tests here.
 
 
@@ -11,45 +11,3 @@ class EntityTest(TestCase):
             name="entity_test",
             time=None
         )
-
-        cls.entity_human = Entity.objects.create(
-            name="entity_human",
-            time=None
-        )
-        cls.human = Human.objects.create(
-            entity=cls.entity_human
-        )
-
-        cls.entity_object = Entity.objects.create(
-            name="entity_object",
-            time=None
-        )
-
-        cls.object = Object.objects.create(
-            entity=cls.entity_object
-        )
-
-        cls.entity_artag = Entity.objects.create(
-            name="entity_artag",
-            time=None
-        )
-
-        cls.ar_tag = ArTag.objects.create(
-            entity=cls.entity_artag
-        )
-
-    def testEntityCreation(self):
-        entity = self.entity
-        self.assertEqual(entity.name, "entity_test")
-
-    def testHumanCreation(self):
-        human = self.human
-        self.assertEqual(human.entity.name, "entity_human")
-
-    def testObjectCreation(self):
-        object = self.object
-        self.assertEqual(object.entity.name, "entity_object")
-
-    def testArTagCreation(self):
-        ar_tag = self.ar_tag
-        self.assertEqual(ar_tag.entity.name, "entity_artag")
