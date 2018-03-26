1a3ab84b090cfa5acfcf9e980690fb694b40ad40
Lucas Maurice <lucas.maurice@outlook.com>
2018-03-25 21:16:58 -0400

    Change serializer for new entity.

--- api/serializers.py
+++ api/serializers.py
@@ -1,41 +1,9 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import Entity, Human, Object, Room, Waypoint, ArTag
+from .models import Entity
 
 
 class EntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entity
-        fields = ('id','name', 'time', 'x_position', 'y_position', 'z_position')
-
-
-class HumanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Human
-        fields = ('id','name', 'gender', 'x_position', 'y_position', 'z_position')
-
-
-class ObjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Object
-        fields = ('id', 'name', 'type', 'color', 'room',  'x_position', 'y_position', 'z_position', 'theta')
-
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = (
-            'id', 'name', 'type',  'x_position', 'y_position', 'z_position'
-        )
-
-
-class WaypointSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Waypoint
-        fields = ('id', 'name', 'x_position', 'y_position')
-
-
-class ArTagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ArTag
-        fields = ('id', 'name', 'ar_id',  'x_position', 'y_position', 'z_position', 'theta')
+        fields = ('id', 'name', 'created', 'modified', 'x_position', 'y_position', 'z_position', 'yaw', 'pitch', 'roll', 'color', 'secondColor', 'thirdColor', 'fourthColor', 'gender')
