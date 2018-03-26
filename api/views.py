00c5597d1eb905476712d5e3de1c76cc817e303e
Lucas Maurice <lucas.maurice@outlook.com>
2018-03-25 21:19:33 -0400

    Prepare views for new entity functions

--- api/views.py
+++ api/views.py
@@ -1,16 +1,8 @@
-from api.models import Entity, Human, Object, Room, Waypoint, ArTag
-from api.serializers import EntitySerializer, HumanSerializer
-from api.serializers import ObjectSerializer, RoomSerializer
-from api.serializers import WaypointSerializer, ArTagSerializer
+from api.models import Entity
+from api.serializers import EntitySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import Http404
-from datetime import datetime
-from django.utils import timezone
-from django.core.exceptions import ObjectDoesNotExist
-import manage as main
-
 
 class EntityList(APIView):
 
@@ -23,117 +15,6 @@ class EntityList(APIView):
 
     def post(self, request, format=None):
         serializer = EntitySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class HumanList(APIView):
-
-    ''' List all Human. '''
-
-    def get(self, request, format=None):
-        humans = Human.objects.all()
-        serializer = HumanSerializer(humans, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = HumanSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ObjectList(APIView):
-
-    ''' List all Object. '''
-
-    def get(self, request, format=None):
-        name = request.query_params.get('name', None)
-        color = request.query_params.get('color', None)
-        room = request.query_params.get('room', None)
-        type = request.query_params.get('type', None)
-        id = request.query_params.get('id', None)
-
-        objects = Object.objects.all()
-        if name is not None:
-            objects = objects.filter(name__icontains=str(name))
-        if color is not None:
-            objects = objects.filter(color__icontains=str(color))
-        if room is not None:
-            objects = objects.filter(room__name__icontains=str(room))
-        if type is not None:
-            objects = objects.filter(type__icontains=str(type))
-        if id is not None:
-            objects = objects.filter(id__exact=id)
-
-        #objects = Object.objects.all()
-        serializer = ObjectSerializer(objects, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ObjectSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RoomList(APIView):
-
-    def get(self, request, format=None):
-
-	name = request.query_params.get('name', None)
-        type = request.query_params.get('type', None)
-        id = request.query_params.get('id', None)
-
-        rooms = Room.objects.all()
-
-        if name is not None:
-            rooms = rooms.filter(name__icontains=str(name))
-        if type is not None:
-            rooms = rooms.filter(type__icontains=str(type))
-        if id is not None:
-            rooms = rooms.filter(id__exact=id)
-
-        serializer = RoomSerializer(rooms, many=True)
-
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = RoomSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class WaypointList(APIView):
-
-    def get(self, request, format=None):
-        waypoints = Waypoint.objects.all()
-        serializer = WaypointSerializer(waypoints, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = WaypointSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ArTagList(APIView):
-
-    def get(self, request, format=None):
-        artag = ArTag.objects.all()
-        serializer = ArTagSerializer(artag, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ArTagSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
