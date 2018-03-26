22cce60f368356db9024287681e7b0f916ce1b4d
Lucas Maurice <lucas.maurice@outlook.com>
2018-03-25 21:15:51 -0400

    Change Urls for match with api needs.

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,5 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^api/entity/$', views.EntityList.as_view()),
-    url(r'^api/human/$', views.HumanList.as_view()),
-    url(r'^api/object/$', views.ObjectList.as_view()),
-    url(r'^api/room/$', views.RoomList.as_view()),
-    url(r'^api/waypoint/$', views.WaypointList.as_view()),
-    url(r'^api/artag/$', views.ArTagList.as_view()),
+    url(r'^api/entity/$', views.EntityList.as_view())
 ]
