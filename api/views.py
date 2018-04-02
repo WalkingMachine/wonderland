0261110511f4b2a55e8da74f1efb87dad9f4574c
Lucas Maurice <lucas.maurice@outlook.com>
2018-04-01 21:47:17 -0400

    Implement a way for find the room corresponding to a location in the arena (x,y).

--- api/views.py
+++ api/views.py
@@ -24,11 +24,36 @@ class EntityList(APIView):
 class SpaceList(APIView):
     # List all Entity or create a new entity.
     def get(self, request, format=None):
-        entities = Space.objects.all()
-        serializer = SpaceSerializer(entities, many=True)
+        objects = Space.objects.all()
+
+        # Read parameters
+        space_id = request.query_params.get('space_id', None)
+        name = request.query_params.get('name', None)
+        x_position = request.query_params.get('x_position', None)
+        y_position = request.query_params.get('y_position', None)
+
+        print (format)
+
+        if space_id is not None:
+            # If there is an ID parameter, return the corespondent rooms
+            objects = objects.filter(space_id__exact=space_id)
+
+        elif name is not None:
+            # If there is a name parameter, return the corespondent rooms
+            objects = objects.filter(name__icontains=str(name))
+
+        elif x_position is not None and y_position is not None:
+            # If there is position parameters, return the rooms corresponding to the position
+            objects = objects.filter(x_left__lte=x_position,
+                                     x_right__gte=x_position,
+                                     y_top__gte=y_position,
+                                     y_bottom__lte=y_position)
+
+        serializer = SpaceSerializer(objects, many=True)
 
         return Response(serializer.data)
 
+    # Add a room in the arena
     def post(self, request, format=None):
         serializer = SpaceSerializer(data=request.data)
 
