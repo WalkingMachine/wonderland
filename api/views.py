829431d246beb37c7f4a3ce7f8f4670bddb09c5b
Lucas Maurice <lucas.maurice@outlook.com>
2018-04-28 00:51:10 -0400

    Make work the new model with up to one container and one room. And 3 nested containers/room.

--- api/views.py
+++ api/views.py
@@ -1,100 +1,62 @@
-from api.models import Entity, Area, Waypoint
-from api.serializers import EntitySerializer, AreaSerializer, WaypointSerializer
+from .models import Entity
+from .serializers import EntitySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Q
 
 
 class EntityList(APIView):
-
     # List all Entity or create a new entity.
     def get(self, request, format=None):
-        entities = Entity.objects.all()
-        serializer = EntitySerializer(entities, many=True)
-        return Response(serializer.data)
+        objects = Entity.objects.all()
+        entity_class = request.query_params.get('entityClass', None)
+        entity_container = request.query_params.get('entityContainer', None)
+        entity_room = request.query_params.get('entityRoom', None)
+        entity_id = request.query_params.get('entityId', None)
 
-    def post(self, request, format=None):
-        serializer = EntitySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if entity_id is not None:
+            print "ID:" + entity_id
+            objects = objects.filter(entityId__iexact=str(entity_id))
 
+        else:
+            # Filter by asked class
+            if entity_class is not None:
+                print "class"
+                objects = objects.filter(entityClass__iexact=str(entity_class))
 
-class AreaList(APIView):
-    # List all Entity or create a new entity.
-    def get(self, request, format=None):
-        objects = Area.objects.all()
+            # Filter by asked container
+            if entity_container is not None:
+                print "container"
+                objects = objects.filter(
+                    Q(entityContainer__entityClass__iexact=str(entity_container)) |
+                    Q(entityContainer__entityContainer__entityClass__iexact=str(entity_container)) |
+                    Q(entityContainer__entityContainer__entityContainer__entityClass__iexact=str(entity_container)))
 
-        # Read parameters
-        area_id = request.query_params.get('area_id', None)
-        name = request.query_params.get('name', None)
-        x_position = request.query_params.get('x_position', None)
-        y_position = request.query_params.get('y_position', None)
+            # Filter by asked room
+            if entity_room is not None:
+                print "room"
+                objects = objects.filter(
+                    Q(Q(entityContainer__entityClass__iexact=str(entity_room)),
+                      Q(entityContainer__entityIsRoom__exact=True)) |
+                    Q(Q(entityContainer__entityContainer__entityClass__iexact=str(entity_room)),
+                      Q(entityContainer__entityContainer__entityIsRoom__exact=True)) |
+                    Q(Q(entityContainer__entityContainer__entityContainer__entityClass__iexact=str(entity_room)),
+                      Q(entityContainer__entityContainer__entityContainer__entityIsRoom__exact=True))
+                )
 
-        if area_id is not None:
-            # If there is an ID parameter, return the corespondent rooms
-            objects = objects.filter(area_id__exact=area_id)
-
-        elif name is not None:
-            # If there is a name parameter, return the corespondent rooms
-            objects = objects.filter(name__icontains=str(name))
-
-        elif x_position is not None and y_position is not None:
-            # If there is position parameters, return the rooms corresponding to the position
-            objects = objects.filter(x_left__lte=x_position,
-                                     x_right__gte=x_position,
-                                     y_top__gte=y_position,
-                                     y_bottom__lte=y_position)
-
-        serializer = AreaSerializer(objects, many=True)
-
+        serializer = EntitySerializer(objects, many=True)
         return Response(serializer.data)
 
     # Add a room in the arena
     def post(self, request, format=None):
-
+        # TODO: ADD THIS PART
         data = request.data
 
         if not data._mutable:
             data._mutable = True
 
-        data['x_left'] = float(data['x_left'])
-        data['x_right'] = float(data['x_right'])
-        data['y_bottom'] = float(data['y_bottom'])
-        data['y_top'] = float(data['y_top'])
-
-        if data['x_left'] > data['x_right']:
-            buffer = data['x_left']
-            data['x_left'] = data['x_right']
-            data['x_right'] = buffer
-
-        if data['y_bottom'] > data['y_top']:
-            buffer = data['y_bottom']
-            data['y_bottom'] = data['y_top']
-            data['y_top'] = buffer
-
-        serializer = AreaSerializer(data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class WaypointList(APIView):
-    # List all Entity or create a new entity.
-    def get(self, request, format=None):
-        objects = Waypoint.objects.all()
-
-        serializer = WaypointSerializer(objects, many=True)
-
-        return Response(serializer.data)
-
-    # Add a room in the arena
-    def post(self, request, format=None):
-        serializer = AreaSerializer(data=request.data)
+        serializer = EntitySerializer(data=data)
 
         if serializer.is_valid():
             serializer.save()
