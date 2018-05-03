fa639e041362567b655e58a94e8ba2b7df29bc9f
Lucas Maurice <lucas.maurice@outlook.com>
2018-05-02 20:52:20 -0400

    Adapt test for new container filter.

--- api/tests.py
+++ api/tests.py
@@ -7,32 +7,29 @@ import json
 
 class EntityTest(TestCase):
     def setUp(self):
-        e1 = Entity.objects.create(entityClass="dining room", entityContainer=None, entityIsRoom=True,
+        e1 = Entity.objects.create(entityClass="dining room", entityContainer=None,
                                    entityIsWaypoint=True, entityWaypointX=0, entityWaypointY=5)         # 1
-        e2 = Entity.objects.create(entityClass="bedroom", entityContainer=None, entityIsRoom=True,
-                                   entityIsWaypoint=True,
+        e2 = Entity.objects.create(entityClass="bedroom", entityContainer=None, entityIsWaypoint=True,
                                    entityWaypointX=12, entityWaypointY=15, entityWaypointYaw=2.3)       # 2
-        e3 = Entity.objects.create(entityClass="table", entityContainer=e1, entityIsRoom=False)         # 3
-        Entity.objects.create(entityClass="chair", entityContainer=e1, entityIsRoom=False)              # 4
-        e5 = Entity.objects.create(entityClass="sideboard", entityContainer=e1, entityIsRoom=False)     # 5
-        Entity.objects.create(entityClass="cut", entityContainer=e3, entityIsRoom=False)                # 6
-        Entity.objects.create(entityClass="apple", entityContainer=e3, entityIsRoom=False,
-                              entityCategory="fruit")                                                   # 7
-        e8 = Entity.objects.create(entityClass="tray", entityContainer=e3, entityIsRoom=False)          # 8
-        Entity.objects.create(entityClass="apple", entityContainer=e8, entityIsRoom=False,
-                              entityCategory="fruit")                                                   # 9
-        Entity.objects.create(entityClass="glass", entityContainer=e5, entityIsRoom=False)              # 10
-        Entity.objects.create(entityClass="glass", entityContainer=e3, entityIsRoom=False)              # 11
-        Entity.objects.create(entityClass="bed", entityContainer=e2, entityIsRoom=False)                # 12
-        e13 = Entity.objects.create(entityClass="night table", entityContainer=e2, entityIsRoom=False)  # 13
-        Entity.objects.create(entityClass="clock", entityContainer=e13, entityIsRoom=False)             # 14
-        Entity.objects.create(entityClass="light", entityContainer=e13, entityIsRoom=False)             # 15
-        e16 = Entity.objects.create(entityClass="table", entityContainer=e2, entityIsRoom=False)        # 16
-        Entity.objects.create(entityClass="cut", entityContainer=e16, entityIsRoom=False)               # 17
-        Entity.objects.create(entityClass="pen", entityContainer=e16, entityIsRoom=False)               # 18
-        Entity.objects.create(entityClass="desk", entityContainer=e2, entityIsRoom=False,
+        e3 = Entity.objects.create(entityClass="table", entityContainer=e1)                             # 3
+        Entity.objects.create(entityClass="chair", entityContainer=e1,)                                 # 4
+        e5 = Entity.objects.create(entityClass="sideboard", entityContainer=e1)                         # 5
+        Entity.objects.create(entityClass="cut", entityContainer=e3,)                                   # 6
+        Entity.objects.create(entityClass="apple", entityContainer=e3, entityCategory="fruit")          # 7
+        e8 = Entity.objects.create(entityClass="tray", entityContainer=e3,)                             # 8
+        Entity.objects.create(entityClass="apple", entityContainer=e8, entityCategory="fruit")          # 9
+        Entity.objects.create(entityClass="glass", entityContainer=e5)                                  # 10
+        Entity.objects.create(entityClass="glass", entityContainer=e3)                                  # 11
+        Entity.objects.create(entityClass="bed", entityContainer=e2)                                    # 12
+        e13 = Entity.objects.create(entityClass="night table", entityContainer=e2)                      # 13
+        Entity.objects.create(entityClass="clock", entityContainer=e13)                                 # 14
+        Entity.objects.create(entityClass="light", entityContainer=e13)                                 # 15
+        e16 = Entity.objects.create(entityClass="table", entityContainer=e2)                            # 16
+        Entity.objects.create(entityClass="cut", entityContainer=e16)                                   # 17
+        Entity.objects.create(entityClass="pen", entityContainer=e16)                                   # 18
+        Entity.objects.create(entityClass="desk", entityContainer=e2,
                               entityIsWaypoint=True, entityWaypointX=-10, entityWaypointY=-15)          # 19
-        Entity.objects.create(entityClass="chair", entityContainer=e2, entityIsRoom=False)              # 20
+        Entity.objects.create(entityClass="chair", entityContainer=e2)                                  # 20
 
     def test_get_by_id_view(self):
         response = self.client.get('/api/entity/', {'entityId': 1})
@@ -89,15 +86,8 @@ class EntityTest(TestCase):
         self.assertEqual(len(entity), 1)
         self.assertEqual(entity[0]['entityId'], 20)
 
-    def test_get_single_object_by_name_in_room(self):
-        response = self.client.get('/api/entity/', {'entityClass': 'cut', 'entityRoom': 'bedroom'})
-        entity = json.loads(response.content)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(entity), 1)
-        self.assertEqual(entity[0]['entityId'], 17)
-
     def test_get_multiple_objects_in_container_room(self):
-        response = self.client.get('/api/entity/', {'entityRoom': 'bedroom', 'entityContainer': 'table'})
+        response = self.client.get('/api/entity/', {'entityContainer': ['bedroom', 'table']})
         entity = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(entity), 2)
