c43040833c23fe7ce8b205a99d7692ff28f369ed
Lucas Maurice <lucas.maurice@outlook.com>
2018-05-26 23:33:53 -0400

    Add some rooms.

--- generate_test_db.py
+++ generate_test_db.py
@@ -1,11 +1,18 @@
 from api.models import Entity, People
 
 # Create Rooms
-lobby = Entity.objects.create(entityClass="lobby", entityContainer=None, entityIsWaypoint=True, entityWaypointX=1.23, entityWaypointY=-5.62, entityWaypointYaw=0.15)                    # 1 ----
-corridor = Entity.objects.create(entityClass="corridor", entityContainer=None, entityIsWaypoint=True, entityWaypointX=1.115, entityWaypointY=-1.09, entityWaypointYaw=-1.57)            # 2 ----
-bedroom = Entity.objects.create(entityClass="bedroom", entityContainer=None, entityIsWaypoint=True, entityWaypointX=3.99, entityWaypointY=-5.50, entityWaypointYaw=0)                   # 3 ----
-livingroom = Entity.objects.create(entityClass="living room", entityContainer=None, entityIsWaypoint=True, entityWaypointX=4.795, entityWaypointY=-0.495, entityWaypointYaw=-2.250)     # 4 ----
-hall = Entity.objects.create(entityClass="hall", entityContainer=None, entityIsWaypoint=True, entityWaypointX=3.47, entityWaypointY=2.63, entityWaypointYaw=-1.57)                      # 5 ----
+hallway = Entity.objects.create(entityClass="lobby", entityContainer=None, entityIsWaypoint=True, entityWaypointX=1.23, entityWaypointY=-5.62, entityWaypointYaw=0.15)                  # 1
+kitchen = Entity.objects.create(entityClass="kitchen", entityContainer=None, entityIsWaypoint=True, entityWaypointX=4.795, entityWaypointY=-0.495, entityWaypointYaw=-2.250)            # 2
+corridor = Entity.objects.create(entityClass="corridor", entityContainer=None, entityIsWaypoint=True, entityWaypointX=1.115, entityWaypointY=-1.09, entityWaypointYaw=-1.57)            # 3
+bedroom = Entity.objects.create(entityClass="bedroom", entityContainer=None, entityIsWaypoint=True, entityWaypointX=3.99, entityWaypointY=-5.50, entityWaypointYaw=0)                   # 4
+bathroom = Entity.objects.create(entityClass="bathroom", entityContainer=None, entityIsWaypoint=True, entityWaypointX=1.115, entityWaypointY=-1.09, entityWaypointYaw=-1.57)            # 5
+lobby = Entity.objects.create(entityClass="lobby", entityContainer=None, entityIsWaypoint=True, entityWaypointX=3.47, entityWaypointY=2.63, entityWaypointYaw=-1.57)                    # 6
+
+# Create main containers
+table = Entity.objects.create(entityClass="table", entityContainer=hallway, entityIsWaypoint=True, entityWaypointX=4.27, entityWaypointY=2.63, entityWaypointYaw=-1.57)                 # 7
+desk = Entity.objects.create(entityClass="desk", entityContainer=hallway, entityIsWaypoint=True, entityWaypointX=3.23, entityWaypointY=-0.02, entityWaypointYaw=-1.57)                  # 8
+counter = Entity.objects.create(entityClass="counter", entityContainer=bathroom)                                                                                                        # 9
+cupboard = Entity.objects.create(entityClass="cupboard", entityContainer=lobby)                                                                                                         # 10
 
 
 
