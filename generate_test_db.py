754a4a490fd0d16a228454e6c25a981f59470e36
Jeffrey Cousineau <jeffrey.cousineau@gmail.com>
2018-05-28 22:21:32 -0400

    Update generate_test_db.py

--- generate_test_db.py
+++ generate_test_db.py
@@ -29,7 +29,7 @@ Entity.objects.create(entityClass="chair", entityContainer=e2)  # 20
 People.objects.create(peopleRecognitionId=1, peopleAge=15, peopleColor="blue", peoplePose="lying", peoplePoseAccuracy=12.3,
                       peopleGender="boy", peopleGenderAccuracy=100.0, peopleIsOperator=False, peopleName="Jeff")
 People.objects.create(peopleRecognitionId=10, peopleAge=25, peopleColor="red", peoplePose="sitting", peoplePoseAccuracy=45.6,
-                      peopleGender="girl", peopleGenderAccuracy=100.0, peopleIsOperator=True, peopleName="Julie")
+                      peopleGender="girl", peopleGenderAccuracy=100.0, peopleIsOperator=True, peopleName="Annie-Anh")
 People.objects.create(peopleRecognitionId=45, peopleAge=43, peopleColor="green", peoplePose="pointing left", peoplePoseAccuracy=78.9,
                       peopleGender="boy", peopleGenderAccuracy=100.0, peopleIsOperator=False, peopleName="Phil")
 People.objects.create(peopleRecognitionId=20, peopleAge=18, peopleColor="green", peoplePose="standing", peoplePoseAccuracy=78.9,
